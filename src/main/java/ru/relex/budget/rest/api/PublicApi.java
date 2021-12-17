b8616e88a42014f4c36551d6348c24a4795107aa
Дмитрий <dmitriy-ershov-01@ya.ru>
2021-12-17 18:51:08 +0300

    added test helloworld контроллер

--- src/main/java/ru/relex/budget/rest/api/PublicApi.java
+++ src/main/java/ru/relex/budget/rest/api/PublicApi.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.relex.budget.services.facade.TransactionFacade;
@@ -83,6 +84,13 @@ public class PublicApi {
     //возвращает html страничку с homePage
     return "Welcome to Budget Adviser";
   }
+
+
+  @GetMapping(value = "/hello")
+  public ResponseEntity<String> testHello() {
+    return new ResponseEntity<String>("Hello world", HttpStatus.OK);
+  }
+
 
   @PostMapping
   @RequestMapping(
