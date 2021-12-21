5ce9c1a1024f477fd96dbcfdec49d19f2b00677f
Дмитрий <dmitriy-ershov-01@ya.ru>
2021-12-21 16:43:42 +0300

    пощады

--- src/main/java/ru/relex/budget/rest/App.java
+++ src/main/java/ru/relex/budget/rest/App.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 import ru.relex.budget.services.ServicesConfiguration;
+import ru.relex.budget.security.SecurityConfig;
 
 @SpringBootApplication
 @Import({
   ServicesConfiguration.class,
-  ru.relex.budget.security.SecurityConfig.class
+  SecurityConfig.class
 })
 public class App {
   public static void main(String[] args) {
